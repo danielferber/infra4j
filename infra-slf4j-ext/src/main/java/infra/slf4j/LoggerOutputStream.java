6354bd0f68722215fd3505f284e05144ffee03e1
Daniel Felix Ferber <danielferber@gmail.com>
2012-02-10 17:59:14 -0200

    [fix] LoggerOutputStream.flush() does not log content anymore.

--- infra-slf4j-ext/src/main/java/infra/slf4j/LoggerOutputStream.java
+++ infra-slf4j-ext/src/main/java/infra/slf4j/LoggerOutputStream.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2012 Daniel Felix Ferber
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -47,7 +47,7 @@ public abstract class LoggerOutputStream extends OutputStream {
 	/** Logger para onde será escrito o conteúdo. */
 	protected final Logger logger;
 	/** Buffer que acumula temporariamente o conteúdo. */
-	private final ByteArrayOutputStream os = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream os = new ByteArrayOutputStream(0x3FFF);
 
 	/**
 	 * Construtor padrão. As instâncias de fato devem implementar {@link #writeToLogger()} de acordo com a prioridade do
@@ -71,9 +71,9 @@ public abstract class LoggerOutputStream extends OutputStream {
 
 	@Override
 	public void flush() throws IOException {
-		os.flush();
-		writeToLogger();
-		os.reset();
+//		os.flush();
+//		writeToLogger();
+//		os.reset();
 	}
 
 	@Override
