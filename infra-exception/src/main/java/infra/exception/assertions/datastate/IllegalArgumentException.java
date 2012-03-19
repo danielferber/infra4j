3142ddbfc3cc0d7b16f7d700b09850830390e1c4
Daniel Felix Ferber <danielferber@gmail.com>
2012-03-19 18:19:04 -0300

    [dev] More apply() methods.

--- infra-exception/src/main/java/infra/exception/assertions/datastate/IllegalArgumentException.java
+++ infra-exception/src/main/java/infra/exception/assertions/datastate/IllegalArgumentException.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2012 Daniel Felix Ferber
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,6 +25,7 @@ public class IllegalArgumentException extends IllegalDataStateException {
 
 	public IllegalArgumentException() { super(); }
 	public IllegalArgumentException(String message) { super(message); }
+	protected IllegalArgumentException(SystemRule sr) {	super(sr); }
 
 	/** Raises exception if condition is false. */
 	public static boolean apply(boolean condition) {
@@ -38,4 +39,17 @@ public class IllegalArgumentException extends IllegalDataStateException {
 		}
 		return true;
 	}
+	/** Raises exception if condition is false. */
+	public static boolean apply(SystemRule sr, boolean condition) {
+		if (!condition) throw new IllegalArgumentException(sr);
+		return true;
+	}
+	/** Raises exception if one condition is false. */
+	public static boolean apply(SystemRule sr, boolean ... conditions) {
+		for (boolean b : conditions) {
+			if (!b) throw new IllegalArgumentException(sr);
+		}
+		return true;
+	}
+
 }
--- infra-exception/src/main/java/infra/exception/assertions/datastate/NullArgumentException.java
+++ infra-exception/src/main/java/infra/exception/assertions/datastate/NullArgumentException.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2012 Daniel Felix Ferber
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,6 +24,7 @@ public class NullArgumentException extends IllegalArgumentException {
 
 	public NullArgumentException() { super(); }
 	public NullArgumentException(String message) { super(message); }
+	protected NullArgumentException(SystemRule sr) { super(sr); }
 
 	/** Raises exception any parameter is null. */
 	public static boolean apply(Object... values) {
@@ -32,4 +33,12 @@ public class NullArgumentException extends IllegalArgumentException {
 		}
 		return true;
 	}
+	/** Raises exception any parameter is null. */
+	public static boolean apply(SystemRule sr, Object... values) {
+		for (Object value : values) {
+			if (value == null) throw new NullArgumentException(sr);
+		}
+		return true;
+	}
+
 }
