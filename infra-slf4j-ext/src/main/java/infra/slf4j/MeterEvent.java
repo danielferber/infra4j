898df373968b7a806d1a6efe46bd84f4e84697c1
Daniel Felix Ferber <dff4321@gmail.com>
2012-08-28 21:46:05 -0300

    [enh] Getter for Meter context.

--- infra-slf4j-ext/src/main/java/infra/slf4j/MeterEvent.java
+++ infra-slf4j-ext/src/main/java/infra/slf4j/MeterEvent.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2012 Daniel Felix Ferber
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,7 @@ package infra.slf4j;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -104,6 +105,7 @@ public class MeterEvent implements Serializable {
 	@Column(name="valor", length=100)
 	@CollectionTable(name="contexto_meter", joinColumns=@JoinColumn(name="meterevent_ref"))
 	protected Map<String, String> context;
+	public Map<String, String> getContext() { return Collections.unmodifiableMap(context); }
 
 	public String getName() { return name; }
 	public String getMessage() { return message; }
